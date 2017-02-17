5ab37469e57704093fbc783660973206d7054632
Friedrich von Never <friedrich@fornever.me>
2017-02-17 22:41:26 +0700

    #21: add a simple parser test

---
+++ WpfMath.Tests/ParserTests.cs
@@ -0,0 +1,35 @@
+﻿using DeepEqual.Syntax;
+using Xunit;
+
+namespace WpfMath.Tests
+{
+    public class ParserTests
+    {
+        private readonly TexFormulaParser _parser = new TexFormulaParser();
+
+        static ParserTests()
+        {
+            TexFormulaParser.Initialize();
+        }
+
+        [Fact]
+        public void SimpleAtomList()
+        {
+            var formula = _parser.Parse("2+2");
+            var expected = new TexFormula
+            {
+                RootAtom = new RowAtom
+                {
+                    Elements =
+                    {
+                        new CharAtom('2'),
+                        new SymbolAtom("plus", TexAtomType.BinaryOperator, false),
+                        new CharAtom('2')
+                    }
+                }
+            };
+
+            formula.WithDeepEqual(expected).ExposeInternalsOf<TexFormula>().Assert();
+        }
+    }
+}
---
+++ WpfMath.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WpfMath.Tests")]
+[assembly: AssemblyProduct("WpfMath.Tests")]
+[assembly: AssemblyCopyright("Copyright © wpf-math contributors 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b9f013a3-edd1-408f-81a8-2560db64fa3f")]
+[assembly: AssemblyVersion("0.2.0.*")]
