88a7ea955d41ba867b2eafe3b96eeca883434266
Friedrich von Never <friedrich@fornever.me>
2017-02-18 12:51:47 +0700

    #21: translate tests to F#

---
+++ WpfMath.Tests/Utils.fs
@@ -0,0 +1,11 @@
+﻿module WpfMath.Tests.Utils
+
+open WpfMath
+
+let formula (atoms : Atom seq) : TexFormula =
+    let root = RowAtom()
+    root.Elements.AddRange(atoms)
+    TexFormula(RootAtom = root)
+
+let char (c : char) : CharAtom = CharAtom(c)
+let plus : SymbolAtom = SymbolAtom("plus", TexAtomType.BinaryOperator, false)
