8764e7fa63324fa9de3b23c5673b4b6676091b33
Friedrich von Never <friedrich@fornever.me>
2017-02-18 12:51:47 +0700

    #21: translate tests to F#

---
+++ WpfMath.Tests/AssemblyInfo.fs
@@ -0,0 +1,14 @@
+﻿namespace WpfMath.Tests.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("WPF-Math Tests")>]
+[<assembly: AssemblyProduct("WPF-Math")>]
+[<assembly: AssemblyCopyright("Copyright © wpf-math contributors 2017")>]
+[<assembly: ComVisible(false)>]
+[<assembly: Guid("f19e5fb1-ee6b-4a48-a522-41f30676be41")>]
+[<assembly: AssemblyVersion("0.2.0.*")>]
+[<assembly: AssemblyFileVersion("0.2.0.*")>]
+
+()
---
+++ WpfMath.Tests/ParserTests.fs
@@ -0,0 +1,21 @@
+﻿namespace WpfMath.Tests
+
+open DeepEqual.Syntax
+open Xunit
+
+open WpfMath
+open WpfMath.Tests.Utils
+
+type ParserTests() =
+    do TexFormulaParser.Initialize()
+
+    let assertParseResult formula expected =
+        let parser = TexFormulaParser()
+        let result = parser.Parse(formula)
+        result.WithDeepEqual(expected).ExposeInternalsOf<TexFormula>().Assert()
+
+    [<Fact>]
+    let ``2+2 should be parsed properly`` () =
+        assertParseResult
+        <| "2+2"
+        <| (formula [char '2'; plus; char '2'])
