9b2b53c1d487eaabc92e18c54dfad2c0b99ba980
Evgeniy Andreev (gsomix) <gsomix@gmail.com>
2017-02-20 22:01:39 +0400

    FormulaControl: Property holding parse error

--- WpfMath/Controls/FormulaControl.xaml.cs
+++ WpfMath/Controls/FormulaControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -10,15 +12,37 @@ namespace WpfMath.Controls
     public partial class FormulaControl : UserControl
     {
         private static TexFormulaParser formulaParser = new TexFormulaParser();
+        private TexFormula texFormula; 
 
         public string Formula
         {
-            get { return (string) GetValue(FormulaProperty); }
+            get { return (string)GetValue(FormulaProperty); }
             set { SetValue(FormulaProperty, value); }
         }
 
+        public bool HasError
+        {
+            get { return (bool)GetValue(HasErrorProperty); }
+            private set { SetValue(HasErrorProperty, value); }
+        }
+
+        public ObservableCollection<Exception> Errors
+        {
+            get { return (ObservableCollection<Exception>)GetValue(ErrorsProperty); }
+            private set { SetValue(ErrorsProperty, value); }
+        }
+
         public static readonly DependencyProperty FormulaProperty = DependencyProperty.Register(
-          "Formula", typeof(string), typeof(FormulaControl), new PropertyMetadata("", OnFormulaChanged), ValidateFormula);
+            "Formula", typeof(string), typeof(FormulaControl), 
+            new PropertyMetadata("", OnFormulaChanged, CoerceFormula));
+
+        public static readonly DependencyProperty HasErrorProperty = DependencyProperty.Register(
+            "HasError", typeof(bool), typeof(FormulaControl),
+            new PropertyMetadata(false));
+
+        public static readonly DependencyProperty ErrorsProperty = DependencyProperty.Register(
+            "Errors", typeof(ObservableCollection<Exception>), typeof(FormulaControl),
+            new PropertyMetadata(new ObservableCollection<Exception>()));
 
         public FormulaControl()
         {
@@ -28,12 +52,15 @@ namespace WpfMath.Controls
         private void Render()
         {
             // Create formula object from input text.
-            var formula = formulaParser.Parse(Formula);
-            if (formula == null) return;
+            if (texFormula == null)
+            {
+                formulaContainerElement.Visual = null;
+                return;
+            }
 
             // Render formula to visual.
             var visual = new DrawingVisual();
-            var renderer = formula.GetRenderer(TexStyle.Display, 20d);
+            var renderer = texFormula.GetRenderer(TexStyle.Display, 20d);
             var formulaSize = renderer.RenderSize;
 
             using (var drawingContext = visual.RenderOpen())
@@ -43,15 +70,24 @@ namespace WpfMath.Controls
             formulaContainerElement.Visual = visual;
         }
 
-        private static bool ValidateFormula(object value)
+        private static object CoerceFormula(DependencyObject d, object baseValue)
         {
-            var formula = (string) value;
+            var control = (FormulaControl)d;
+            var formula = (string)baseValue;
             try
             {
-                formulaParser.Parse(formula);
-                return true;
+                control.HasError = false;
+                control.Errors.Clear();
+                control.texFormula = formulaParser.Parse(formula);
+                return baseValue;
             }
-            catch { return false; }
+            catch (TexParseException e)
+            {
+                control.Errors.Add(e);
+                control.HasError = true;
+                control.texFormula = null;
+                return "";
+            }
         }
 
         private static void OnFormulaChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
