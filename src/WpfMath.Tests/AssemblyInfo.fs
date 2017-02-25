5a5ac3463eb0d9119869fb384914ee2afde3ee5c
Friedrich von Never <friedrich@fornever.me>
2017-02-25 14:03:43 +0700

    AssemblyInfo and nuspec: update texts

--- src/WpfMath.Tests/AssemblyInfo.fs
+++ src/WpfMath.Tests/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyTitle("WPF-Math Tests")>]
 [<assembly: AssemblyProduct("WPF-Math")>]
-[<assembly: AssemblyCopyright("Copyright © wpf-math contributors 2017")>]
+[<assembly: AssemblyCopyright("Copyright © WPF-Math Contributors 2017")>]
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("f19e5fb1-ee6b-4a48-a522-41f30676be41")>]
 [<assembly: AssemblyVersion("0.3.0.*")>]
--- src/WpfMath/Properties/AssemblyInfo.cs
+++ src/WpfMath/Properties/AssemblyInfo.cs
@@ -3,9 +3,9 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("WPF-Math")]
-[assembly: AssemblyDescription("Tools for rendering mathematical expressions on-the-fly using the Windows Presentation Foundation.")]
+[assembly: AssemblyDescription(".NET library for rendering mathematical formulae using the LaTeX typsetting style, for the WPF framework.")]
 [assembly: AssemblyProduct("WPF-Math")]
-[assembly: AssemblyCopyright("Copyright © Alex Regueiro 2010; Copyright © wpf-math contributors 2017")]
+[assembly: AssemblyCopyright("Copyright © Alex Regueiro 2010; Copyright © WPF-Math Contributors 2017")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ba920749-5f71-4bf9-8361-9d9f27bec421")]
 [assembly: AssemblyVersion("0.3.0.*")]
