af1ed97eb58c68c84040fa773e5a5c1a821f0ba9
Friedrich von Never <friedrich@fornever.me>
2017-09-10 18:52:37 +0700

    #88: fix accent alignment
    AccentBox should always search for the most nested CharBox when
    determining its' skew.

---
+++ src/WpfMath.Tests/BoxTests.fs
@@ -0,0 +1,23 @@
+namespace WpfMath.Tests
+
+open DeepEqual.Syntax
+open Xunit
+
+open WpfMath
+open WpfMath.Exceptions
+open WpfMath.Tests.Utils
+
+type BoxTests() =
+    [<Fact>]
+    let ``AccentedAtom should have a skew according to the char`` () =
+        let parser = TexFormulaParser()
+        let result = parser.Parse @"\bar{\bar{x}}"
+        let topAtom = result.RootAtom :?> AccentedAtom
+        let childAtom = topAtom.BaseAtom :?> AccentedAtom
+
+        let font = DefaultTexFont 20.0
+        let environment = TexEnvironment(TexStyle.Display, font, font)
+        let topBox = topAtom.CreateBox(environment).Children.[0]
+        let childBox = childAtom.CreateBox(environment).Children.[0]
+
+        Assert.Equal(topBox.Shift, childBox.Shift)
