b67de53d5ac4f8b0a3f60b624f04fb36765b1d08
Friedrich von Never <friedrich@fornever.me>
2017-12-02 20:05:25 +0700

    Tests: make test functions public

--- src/WpfMath.Tests/BoxTests.fs
+++ src/WpfMath.Tests/BoxTests.fs
@@ -1,4 +1,4 @@
-namespace WpfMath.Tests
+module WpfMath.Tests.BoxTests
 
 open DeepEqual.Syntax
 open Xunit
@@ -7,17 +7,16 @@ open WpfMath
 open WpfMath.Exceptions
 open WpfMath.Tests.Utils
 
-type BoxTests() =
-    [<Fact>]
-    let ``AccentedAtom should have a skew according to the char`` () =
-        let parser = TexFormulaParser()
-        let result = parser.Parse @"\bar{\bar{x}}"
-        let topAtom = result.RootAtom :?> AccentedAtom
-        let childAtom = topAtom.BaseAtom :?> AccentedAtom
+[<Fact>]
+let ``AccentedAtom should have a skew according to the char``() =
+    let parser = TexFormulaParser()
+    let result = parser.Parse @"\bar{\bar{x}}"
+    let topAtom = result.RootAtom :?> AccentedAtom
+    let childAtom = topAtom.BaseAtom :?> AccentedAtom
 
-        let font = DefaultTexFont 20.0
-        let environment = TexEnvironment(TexStyle.Display, font, font)
-        let topBox = topAtom.CreateBox(environment).Children.[0]
-        let childBox = childAtom.CreateBox(environment).Children.[0]
+    let font = DefaultTexFont 20.0
+    let environment = TexEnvironment(TexStyle.Display, font, font)
+    let topBox = topAtom.CreateBox(environment).Children.[0]
+    let childBox = childAtom.CreateBox(environment).Children.[0]
 
-        Assert.Equal(topBox.Shift, childBox.Shift)
+    Assert.Equal(topBox.Shift, childBox.Shift)
