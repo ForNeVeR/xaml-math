7d1a6c5016ec924c42b383703b9b0a55176b764c
bezxx_chihuahua <bezaleeloci@gmail.com>
2018-08-08 00:53:17 +0100

    Moved parsers to namespace

--- src/WpfMath/Parsers/PredefinedColorParser.cs
+++ src/WpfMath/Parsers/PredefinedColorParser.cs
@@ -1,12 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Media;
 using System.Xml.Linq;
 using System.Reflection;
+using WpfMath.Utils;    
 
-namespace WpfMath
+namespace WpfMath.Parsers
 {
     internal class PredefinedColorParser
     {
