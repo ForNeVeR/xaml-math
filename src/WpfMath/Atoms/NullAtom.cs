de089aaad572f7aaa7b7f40e68f2d8fd232a7f51
bezxx_chihuahua <bezaleeloci@gmail.com>
2018-08-23 12:02:02 +0100

    Create NullAtom.cs
    This could be used for commands that do something but render nothing(like the definecolor command). Using a strut atom would occupy some space which we might not want.

---
+++ src/WpfMath/Atoms/NullAtom.cs
@@ -0,0 +1,18 @@
+using WpfMath.Boxes;
+
+namespace WpfMath.Atoms
+{
+     internal class NullAtom:Atom
+     {
+          public NullAtom(SourceSpan source=null,TexAtomType type =TexAtomType.Ordinary):base(source, type)
+          {
+          }
+          
+          public static Box NullBox=>new StrutBox(0,0,0,0);
+          
+          protected override Box CreateBoxCore(TexEnvironment environment)
+          {
+              return new StrutBox(0,0,0,0);
+          }
+     }
+}
