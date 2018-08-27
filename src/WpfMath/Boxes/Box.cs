00bac6f96652be9fa155c3d4f015fbc6c7c89b44
bezxx_chihuahua <bezaleeloci@gmail.com>
2018-08-27 11:12:31 +0100

    Added a "Show bounds" property.

--- src/WpfMath/Boxes/Box.cs
+++ src/WpfMath/Boxes/Box.cs
@@ -51,6 +51,11 @@ namespace WpfMath.Boxes
             get;
             set;
         }
+        
+        /// <summary>
+        /// Gets or sets a value that specifies whether the bounds of this box should be shown.
+        /// </summary>
+        public bool ShowBounds{get; set;}
 
         public object Tag{ get; set;}
         
