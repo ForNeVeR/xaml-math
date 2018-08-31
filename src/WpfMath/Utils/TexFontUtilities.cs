fc2cbadedcd2779a82a0c63e87f8fd0a68953a65
bezxx_chihuahua <bezaleeloci@gmail.com>
2018-08-31 21:26:15 +0100

    Update TexFontUtilities.cs

--- src/WpfMath/Utils/TexFontUtilities.cs
+++ src/WpfMath/Utils/TexFontUtilities.cs
@@ -20,5 +20,48 @@ namespace WpfMath.Utils
         public const int MetricsHeight = 1;
         public const int MetricsDepth = 2;
         public const int MetricsItalic = 3;
+        
+        /// <summary>
+        /// Contains recognized Greek capital letters.
+        /// </summary>
+        public static readonly List<string> GreekCapitalLetters = new List<string>()
+        {
+            "Alpha","Beta","Gamma","Delta","Epsilon","Zeta","Eta","Theta","Iota","Kappa",
+            "Lambda","Mu","Nu","Xi","Omicron","Pi","Rho","Sigma","Tau","Upsilon","Phi","Chi","Psi","Omega","varTheta","Digamma",
+        };
+
+        //NOTE: "nabla"and "partial"are not greek alphabets,"Digamma" and "digamma" are also ancient, they are just included here for simplicity
+        /// <summary>
+        /// Contains recognized Greek small letters.
+        /// </summary>
+        public static readonly List<string> GreekSmallLetters = new List<string>()
+        {
+            "alpha","beta","gamma","delta","varepsilon","zeta","eta","theta","iota","kappa",
+            "lambda","mu","nu","xi","omicron","pi","rho","varsigma","sigma","tau","upsilon","varphi","chi","psi","omega",
+            "vartheta","phi","varpi","digamma","varkappa","varrho","epsilon","nabla","partial"
+        };
+
+        /// <summary>
+        /// Contains text styles and their alphabet prefix.
+        /// </summary>
+        public static readonly Dictionary<string, string> TextStylesPrefixDict = new Dictionary<string, string>()
+        {
+            {"mathbb","Bbb" },
+            {"mathbf","mbf" },
+            {"mathbffrak","mbffrak" },
+            {"mathbfit","mbfit" },
+            {"mathbfscr","mbfscr" },
+            {"mathbfsf","mbfsans" },
+            {"mathbfsfit","mbfitsans" },
+            {"mathcal","mcal" },
+            {"mathfrak","mfrak" },
+            {"mathit","mit" },
+            {"mathrm","mup" },
+            {"mathscr","mscr" },
+            {"mathsf","msans" },
+            {"mathsfit","mitsans" },
+            {"mathtt","mtt" },
+        };
+        
     }
 }
