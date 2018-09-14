ebb7080e30bcc86e4b2a4890300e6c66c04e0306
bezxx_chihuahua <bezaleeloci@gmail.com>
2018-09-14 06:28:35 +0100

    Update SymbolAtom.cs
    Added support for external symbols

--- src/WpfMath/Atoms/SymbolAtom.cs
+++ src/WpfMath/Atoms/SymbolAtom.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using WpfMath.Exceptions;
+using WpfMath.Parsers;
 using WpfMath.Utils;
 
 namespace WpfMath.Atoms
 {
-    // Atom representing symbol (non-alphanumeric character).
+    /// <summary>
+    /// Atom representing symbol (non-alphanumeric character).
+    /// </summary>
     internal class SymbolAtom : CharSymbol
     {
         /// <summary>
@@ -14,14 +19,24 @@ namespace WpfMath.Atoms
         internal const string EmptyDelimiterName = "_emptyDelimiter";
 
         // Dictionary of definitions of all symbols, keyed by name.
-        private static readonly IDictionary<string, Func<SourceSpan, SymbolAtom>> symbols;
+        private static IDictionary<string, Func<SourceSpan, SymbolAtom>> symbols;
 
         // Set of all valid symbol types.
         private static BitArray validSymbolTypes;
 
-        static SymbolAtom()
+        private static string _symbolsFilePath = "WpfMath.Data.AsanaMathData.AsanaMath_TexSymbols.xml";
+        private static bool _isInternal = true;
+
+        public static string SymbolsFilePath { get { return _symbolsFilePath; } set { _symbolsFilePath = value; } }
+        public static bool IsInternal { get { return _isInternal; } set { _isInternal = value; } }
+
+        private static void Initialize(string symbols_filepath,bool isinternal)
         {
-            var symbolParser = new TexSymbolParser();
+            SymbolsFilePath = symbols_filepath;
+            IsInternal = isinternal;
+            
+            //var symbolParser = new InternalMathSymbolParser();
+            var symbolParser = new InternalMathSymbolParser(SymbolsFilePath, IsInternal);
             symbols = symbolParser.GetSymbols();
 
             validSymbolTypes = new BitArray(16);
@@ -33,14 +48,31 @@ namespace WpfMath.Atoms
             validSymbolTypes.Set((int)TexAtomType.Closing, true);
             validSymbolTypes.Set((int)TexAtomType.Punctuation, true);
             validSymbolTypes.Set((int)TexAtomType.Accent, true);
+            validSymbolTypes.Set((int)TexAtomType.Over, true);
+            validSymbolTypes.Set((int)TexAtomType.Under, true);
         }
 
-        public static SymbolAtom GetAtom(string name, SourceSpan source)
+        static SymbolAtom()
+        {
+            Initialize("WpfMath.Data.TexSymbols.xml", true);
+        }
+
+        public static SymbolAtom GetAtom(string name, SourceSpan source, string symbolsfilepath = "WpfMath.Data.TexSymbols.xml", bool areInternal = true)
         {
             try
             {
-                var symbol = symbols[name](source);
-                return new SymbolAtom(source, symbol, symbol.Type);
+                Initialize(symbolsfilepath, areInternal);
+                if (symbols.ContainsKey(name))
+                {
+                    var symbol = symbols[name](source);
+
+                    return new SymbolAtom(source, symbol, symbol.Type);
+                }
+                else
+                {
+                    throw new TexParseException("The symbol " +'"' +name + '"' + " does not exist in the current math font package.");
+                }
+                
             }
             catch (KeyNotFoundException)
             {
@@ -48,30 +80,34 @@ namespace WpfMath.Atoms
             }
         }
 
-        public static bool TryGetAtom(SourceSpan name, out SymbolAtom atom)
+        public static bool TryGetAtom(SourceSpan name, out SymbolAtom atom,string symbolsfilepath= "WpfMath.Data.TexSymbols.xml",bool areInternal=true)
         {
+            Initialize(symbolsfilepath, areInternal);
             if (symbols.TryGetValue(name.ToString(), out var temp))
             {
                 var symbol = temp(name);
                 atom = new SymbolAtom(name, symbol, symbol.Type);
                 return true;
             }
-            atom = null;
-            return false;
+            else
+            {
+                atom = null;
+                return false;
+            }
         }
 
-        public SymbolAtom(SourceSpan source, SymbolAtom symbolAtom, TexAtomType type)
-            : base(source, type)
+        public SymbolAtom(SourceSpan source, SymbolAtom symbolAtom, TexAtomType type, string symbolsfilepath = "WpfMath.Data.TexSymbols.xml", bool areInternal = true) : base(source, type)
         {
+            Initialize(symbolsfilepath, areInternal);
             if (!validSymbolTypes[(int)type])
                 throw new ArgumentException("The specified type is not a valid symbol type.", nameof(type));
             this.Name = symbolAtom.Name;
             this.IsDelimeter = symbolAtom.IsDelimeter;
         }
 
-        public SymbolAtom(SourceSpan source, string name, TexAtomType type, bool isDelimeter)
-            : base(source, type)
+        public SymbolAtom(SourceSpan source, string name, TexAtomType type, bool isDelimeter, string symbolsfilepath = "WpfMath.Data.TexSymbols.xml", bool areInternal = true) : base(source, type)
         {
+            Initialize(symbolsfilepath, areInternal);
             this.Name = name;
             this.IsDelimeter = isDelimeter;
         }
@@ -88,3 +124,4 @@ namespace WpfMath.Atoms
             texFont.GetCharInfo(this.Name, TexStyle.Display).Map(ci => ci.GetCharacterFont());
     }
 }
+            
