d865996e0eb4ccf2c4d994c7b0065bd245508678
Friedrich von Never <friedrich@fornever.me>
2019-08-22 23:21:17 +0700

    Update a package publishing script (#17)

--- scripts/nuget-pack.ps1
+++ scripts/nuget-pack.ps1
@@ -1,14 +1,31 @@
 param (
     $SourceDirectory = "$PSScriptRoot/..",
+    $AvaloniaMathDirectory = "$SourceDirectory/src/AvaloniaMath",
+
+    $dotnet = 'dotnet',
     $msbuild = 'msbuild',
     $nuget = 'nuget'
 )
 
+Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
 & $msbuild /m "$SourceDirectory/WpfMath.sln" /p:Configuration=Release /p:Platform="Any CPU" '/t:Clean;Rebuild'
 if (-not $?) {
-    throw "msbuild returned error code: $LASTEXITCODE"
+    throw "msbuild returned an error code: $LASTEXITCODE"
 }
 
 & $nuget pack -BasePath $SourceDirectory "$SourceDirectory/WpfMath.nuspec"
+if (-not $?) {
+    throw "NuGet returned an error code: $LASTEXITCODE"
+}
+
+Push-Location $AvaloniaMathDirectory
+try {
+    & $dotnet pack --configuration Release
+    if (-not $?) {
+        throw "dotnet pack returned an error code: $LASTEXITCODE"
+    }
+} finally {
+    Pop-Location
+}
