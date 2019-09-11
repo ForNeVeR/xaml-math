99defa6587a56f3643f382124a39c6bd351ece4f
Friedrich von Never <friedrich@fornever.me>
2019-09-11 22:47:28 +0700

    Roll back the unnecessary changes in Radical (#128)

--- src/WpfMath/Atoms/Radical.cs
+++ src/WpfMath/Atoms/Radical.cs
@@ -13,27 +13,22 @@ namespace WpfMath
 
         private const double scale = 0.55;
 
-        public Radical(SourceSpan source, Atom baseAtom, Atom degreeAtom = null,bool degreespecified=false): base(source)
+        public Radical(SourceSpan source, Atom baseAtom, Atom degreeAtom = null)
+            : base(source)
         {
             this.BaseAtom = baseAtom;
             this.DegreeAtom = degreeAtom;
-            this.DegreeSpecified = degreespecified;
         }
 
         public Atom BaseAtom { get; }
 
         public Atom DegreeAtom { get; }
-        /// <summary>
-        /// Gets a value that specifies whether an index was requested.
-        /// </summary>
-        public bool DegreeSpecified { get; }
 
         protected override Box CreateBoxCore(TexEnvironment environment)
         {
             var texFont = environment.MathFont;
             var style = environment.Style;
 
-            var axis = texFont.GetAxisHeight(style);
             // Calculate minimum clearance amount.
             double clearance;
             var defaultRuleThickness = texFont.GetDefaultLineThickness(style);
@@ -44,73 +39,48 @@ namespace WpfMath
             clearance = defaultRuleThickness + Math.Abs(clearance) / 4;
 
             // Create box for base atom, in cramped style.
-            var baseBox =this.BaseAtom==null?StrutBox.Empty: this.BaseAtom.CreateBox(environment.GetCrampedStyle());
+            var baseBox = this.BaseAtom.CreateBox(environment.GetCrampedStyle());
 
             // Create box for radical sign.
             var totalHeight = baseBox.Height + baseBox.Depth;
-            var radicalSignBox = DelimiterFactory.CreateBox(sqrtSymbol, totalHeight + clearance + defaultRuleThickness,environment, Source);
+            var radicalSignBox = DelimiterFactory.CreateBox(sqrtSymbol, totalHeight + clearance + defaultRuleThickness,
+                environment, Source);
             radicalSignBox.Source = Source;
 
             // Add half of excess height to clearance.
             var delta = radicalSignBox.Depth - (totalHeight + clearance);
             clearance += delta / 2;
 
-            // Create box for root base box.
-            var overBar = new OverBar(environment, baseBox, clearance, radicalSignBox.Height)
-            {
-                Shift = -defaultRuleThickness,
-            };
+            // Create box for square-root containing base box.
+            radicalSignBox.Shift = -(baseBox.Height + clearance);
+            var overBar = new OverBar(environment, baseBox, clearance, radicalSignBox.Height);
+            overBar.Shift = -(baseBox.Height + clearance + defaultRuleThickness);
+            var radicalContainerBox = new HorizontalBox(radicalSignBox);
+            radicalContainerBox.Add(overBar);
 
-            //Create box to hold the radical and the degree atom (if it exists)
-            var radicalContainerBox = new VerticalBox();
-            //Create a box for creating a space for the radical sign boxs' left shift and width
-            var horizoverlapbox = new StrutBox(0, 0, 0, 0);
-            radicalContainerBox.Add(horizoverlapbox);
-            radicalContainerBox.Add(radicalSignBox);
+            // If atom is simple radical, just return square-root box.
+            if (this.DegreeAtom == null)
+                return radicalContainerBox;
 
-            // Create box for root index
-            var radrootBox = DegreeAtom == null ? StrutBox.Empty : DegreeAtom.CreateBox(environment.GetRootStyle());
-            var bottomShift = scale * (radicalSignBox.Height + radicalSignBox.Depth);
-            var rcbItemsdiff = radicalSignBox.TotalHeight -radrootBox.TotalHeight;
-            bottomShift = rcbItemsdiff;
-            radrootBox.Shift = 0;
+            // Atom is complex radical (nth-root).
 
-            if (radrootBox.TotalHeight < radicalSignBox.TotalHeight / 2)
-                bottomShift = (radicalSignBox.TotalHeight / 2)+radrootBox.TotalHeight;
+            // Create box for root atom.
+            var rootBox = this.DegreeAtom.CreateBox(environment.GetRootStyle());
+            var bottomShift = scale * (radicalContainerBox.Height + radicalContainerBox.Depth);
+            rootBox.Shift = radicalContainerBox.Depth - rootBox.Depth - bottomShift;
 
-            var Vnegspace = new StrutBox(0, -bottomShift, 0, 0);
-            radicalContainerBox.Add(Vnegspace);
-            radicalContainerBox.Add(radrootBox);
-
-            var leftshift = this.DegreeAtom == null ?0: radrootBox.TotalWidth - radicalSignBox.TotalWidth / 2;
-            if (radrootBox.TotalWidth< (radicalSignBox.TotalWidth / 2))
-            {
-                leftshift = 0;
-                radrootBox.Shift = (radicalSignBox.TotalWidth- radrootBox.TotalWidth)/3;
-            }
-            radicalSignBox.Shift = leftshift;
-
-            //increase the left overlap width
-            horizoverlapbox.Width = leftshift + radicalSignBox.Width;
-
-            var enviromentYDiff = axis >= radicalContainerBox.TotalHeight ? -(axis - radicalContainerBox.TotalHeight) / 2 : (radicalContainerBox.TotalHeight - axis) / 2;
             // Create result box.
             var resultBox = new HorizontalBox();
+
+            // Add box for negative kern.
+            var negativeKern = new SpaceAtom(null, TexUnit.Mu, -10, 0, 0).CreateBox(environment);
+            var xPos = rootBox.Width + negativeKern.Width;
+            if (xPos < 0)
+                resultBox.Add(new StrutBox(-xPos, 0, 0, 0));
+
+            resultBox.Add(rootBox);
+            resultBox.Add(negativeKern);
             resultBox.Add(radicalContainerBox);
-            //adjust the vertical shift of the radicalContainerBox
-            radicalContainerBox.Shift = -radicalSignBox.TotalHeight/2 +  2*enviromentYDiff -radicalContainerBox.Depth;
-
-            double leftpad=0.0;
-            if (radrootBox.TotalWidth < radicalSignBox.TotalWidth )
-                leftpad = leftshift>0?leftshift:0;
-
-            if (radrootBox.TotalWidth >= (radicalSignBox.TotalWidth ))
-                leftpad = radicalSignBox.TotalWidth / 2;
-
-            resultBox.Add(new StrutBox(leftpad, 0, 0, 0));
-
-            overBar.Shift = -radicalSignBox.TotalHeight/2 + 2*enviromentYDiff - radicalContainerBox.Depth;
-            resultBox.Add(overBar);
 
             return resultBox;
         }
