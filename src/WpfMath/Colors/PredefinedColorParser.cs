1a69b9d72dd3ba91b9445fec3c9f4b1382543678
Friedrich von Never <friedrich@fornever.me>
2019-10-20 14:50:57 +0700

    Support alpha channel for the predefined colors (#165)

--- src/WpfMath/Colors/PredefinedColorParser.cs
+++ src/WpfMath/Colors/PredefinedColorParser.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Reflection;
 using System.Windows.Media;
 using System.Xml.Linq;
 
 namespace WpfMath.Colors
 {
-    internal class PredefinedColorParser : SingleComponentColorParser
+    internal class PredefinedColorParser : IColorParser
     {
         private const string ResourceName = TexUtilities.ResourcesDataDirectory + "PredefinedColors.xml";
         public static readonly PredefinedColorParser Instance = new PredefinedColorParser(ResourceName);
@@ -22,12 +24,38 @@ namespace WpfMath.Colors
             }
         }
 
-        protected override Color? ParseSingleComponent(string component)
+        public Color? Parse(IEnumerable<string> components)
         {
-            if (_colors.TryGetValue(component, out var color))
-                return color;
+            var componentList = components.ToList();
+            var hasAlphaComponent = componentList.Count == 2;
+            if (componentList.Count != 1 && !hasAlphaComponent)
+                return null;
 
-            return null;
+            var colorName = componentList[0];
+            if (!_colors.TryGetValue(colorName, out var color))
+                return null;
+
+            byte? alpha = 255;
+            if (hasAlphaComponent)
+            {
+                var alphaFraction = double.TryParse(
+                    componentList[1],
+                    NumberStyles.AllowDecimalPoint,
+                    CultureInfo.InvariantCulture,
+                    out var a)
+                    ? (double?) a
+                    : null;
+                if (alphaFraction == null || alphaFraction < 0.0 || alphaFraction > 1.0)
+                    return null;
+
+                alpha = (byte) Math.Round(255.0 * a, MidpointRounding.AwayFromZero);
+            }
+
+            if (alpha == null)
+                return null;
+
+            color.A = alpha.Value;
+            return color;
         }
 
         private Dictionary<string, Color> Parse(XElement rootElement)
