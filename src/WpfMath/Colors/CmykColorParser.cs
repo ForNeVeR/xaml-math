8eee404eb7158e1a9ea2eb609817758e539ef98c
Friedrich von Never <friedrich@fornever.me>
2019-10-20 14:55:09 +0700

    Support alpha channel in the CMYK color parser (#165)

--- src/WpfMath/Colors/CmykColorParser.cs
+++ src/WpfMath/Colors/CmykColorParser.cs
@@ -6,15 +6,16 @@ using System.Windows.Media;
 
 namespace WpfMath.Colors
 {
-    internal class CmykColorParser : FixedComponentCountColorParser
+    internal class CmykColorParser : IColorParser
     {
-        public CmykColorParser() : base(4)
+        public Color? Parse(IEnumerable<string> components)
         {
-        }
+            var componentList = components.ToList();
+            var hasAlpha = componentList.Count == 5;
+            if (componentList.Count != 4 && !hasAlpha)
+                return null;
 
-        protected override Color? ParseComponents(List<string> components)
-        {
-            var cmyk = components.Select(x =>
+            var cmyk = componentList.Select(x =>
             {
                 var success = double.TryParse(
                     x,
@@ -27,13 +28,15 @@ namespace WpfMath.Colors
             var m = cmyk[1];
             var y = cmyk[2];
             var k = cmyk[3];
-            if (c == null || m == null || y == null || k == null)
+            var aFraction = hasAlpha ? cmyk[4] : 1.0;
+            if (c == null || m == null || y == null || k == null || aFraction == null)
                 return null;
 
             var r = (byte) Math.Round(255.0 * (1.0 - c.Value) * (1.0 - k.Value), MidpointRounding.AwayFromZero);
             var g = (byte) Math.Round(255.0 * (1.0 - m.Value) * (1.0 - k.Value), MidpointRounding.AwayFromZero);
             var b = (byte) Math.Round(255.0 * (1.0 - y.Value) * (1.0 - k.Value), MidpointRounding.AwayFromZero);
-            return Color.FromRgb(r, g, b);
+            var a = (byte) Math.Round(255.0 * aFraction.Value, MidpointRounding.AwayFromZero);
+            return Color.FromArgb(a, r, g, b);
         }
     }
 }
