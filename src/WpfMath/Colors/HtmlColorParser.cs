12f5d10ed645920568c9f53ab358f06dc572c73f
Friedrich von Never <friedrich@fornever.me>
2019-10-20 14:20:43 +0700

    Support the alpha channel in the HTML color parser (#165)

--- src/WpfMath/Colors/HtmlColorParser.cs
+++ src/WpfMath/Colors/HtmlColorParser.cs
@@ -7,14 +7,27 @@ namespace WpfMath.Colors
     {
         protected override Color? ParseSingleComponent(string component)
         {
-            if (component.Length != 6)
+            var isRgb = component.Length == 6;
+            var isRgba = component.Length == 8;
+            if (!isRgb && !isRgba)
                 return null;
 
             var colorCode = int.Parse(component, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
-            var r = (byte) ((colorCode & 0xFF0000) >> 16);
-            var g = (byte) ((colorCode & 0xFF00) >> 8);
-            var b = (byte) (colorCode & 0xFF);
-            return Color.FromRgb(r, g, b);
+            if (isRgb)
+            {
+                var r = (byte) ((colorCode & 0xFF0000) >> 16);
+                var g = (byte) ((colorCode & 0xFF00) >> 8);
+                var b = (byte) (colorCode & 0xFF);
+                return Color.FromRgb(r, g, b);
+            }
+            else
+            {
+                var r = (byte) ((colorCode & 0xFF0000) >> 24);
+                var g = (byte) ((colorCode & 0xFF00) >> 16);
+                var b = (byte) ((colorCode & 0xFF) >> 8);
+                var a = (byte) (colorCode & 0xFF);
+                return Color.FromArgb(a, r, g, b);
+            }
         }
     }
 }
