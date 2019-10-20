1e1deb3046d733c8a1ae22a5eebd3815f36ee63d
Friedrich von Never <friedrich@fornever.me>
2019-10-20 14:59:27 +0700

    Support alpha channel in the grayscale color parser (#165)

--- src/WpfMath/Colors/GrayscaleColorParser.cs
+++ src/WpfMath/Colors/GrayscaleColorParser.cs
@@ -1,19 +1,40 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Windows.Media;
 
 namespace WpfMath.Colors
 {
-    internal class GrayscaleColorParser : SingleComponentColorParser
+    internal class GrayscaleColorParser : IColorParser
     {
-        protected override Color? ParseSingleComponent(string component)
+        public Color? Parse(IEnumerable<string> components)
         {
-            var gradation = double.Parse(component, CultureInfo.InvariantCulture);
+            var componentList = components.ToList();
+            var hasAlpha = componentList.Count == 2;
+            if (componentList.Count != 1 && !hasAlpha)
+                return null;
+
+            var gradation = double.Parse(componentList[0], CultureInfo.InvariantCulture);
             if (gradation < 0.0 || gradation > 1.0)
                 return null;
 
+            double? alpha = 1.0;
+            if (hasAlpha)
+                alpha = double.TryParse(
+                            componentList[1],
+                            NumberStyles.AllowDecimalPoint,
+                            CultureInfo.InvariantCulture,
+                            out var value) && value >= 0.0 && value <= 1.0
+                    ? (double?) value
+                    : null;
+
+            if (alpha == null)
+                return null;
+
             var colorValue = (byte) Math.Round(gradation * 255.0, MidpointRounding.AwayFromZero);
-            return Color.FromRgb(colorValue, colorValue, colorValue);
+            var a = (byte) Math.Round(alpha.Value * 255.0, MidpointRounding.AwayFromZero);
+            return Color.FromArgb(a, colorValue, colorValue, colorValue);
         }
     }
 }
