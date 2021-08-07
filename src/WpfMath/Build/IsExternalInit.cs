c66b10c11fa3188a048ccd9ce61adb8c2370175f
Friedrich von Never <friedrich@fornever.me>
2021-08-07 18:59:27 +0700

    (#265) Build: add IsExternalInit class for the init-only properties

---
+++ src/WpfMath/Build/IsExternalInit.cs
@@ -0,0 +1,8 @@
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>This is only used to enable init-only attributes for older target frameworks.</summary>
+    internal class IsExternalInit
+    {
+    }
+}
