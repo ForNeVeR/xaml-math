a13b40760625e19497418ce9da2f6818a8935961
Friedrich von Never <friedrich@fornever.me>
2023-01-07 23:22:29 +0100

    (#63) IPlatformBrush, RgbaColor: port from the Avalonia branch

---
+++ src/WpfMath.Shared/Colors/RgbaColor.cs
@@ -0,0 +1,3 @@
+namespace WpfMath.Colors;
+
+public record struct RgbaColor(byte R, byte G, byte B, byte A = 0xff);
---
+++ src/WpfMath.Shared/Rendering/GenericPlatformBrush.cs
@@ -0,0 +1,13 @@
+namespace WpfMath.Rendering;
+
+public class GenericPlatformBrush<TBrush> : IPlatformBrush
+{
+    private readonly TBrush _brush;
+
+    public GenericPlatformBrush(TBrush brush)
+    {
+        _brush = brush;
+    }
+
+    public TBrush Get() => _brush;
+}
