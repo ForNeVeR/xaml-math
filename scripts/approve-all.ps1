434da606fb3a90d26b0e1aaf63369c769a7b2df6
Friedrich von Never <friedrich@fornever.me>
2023-01-24 23:26:53 +0100

    (#333) approve-all.ps1: support ApiTEst

--- scripts/approve-all.ps1
+++ scripts/approve-all.ps1
@@ -1,13 +1,20 @@
 param (
     $SolutionRoot = "$PSScriptRoot/..",
-    $TestResultDirectory = "$SolutionRoot/src/WpfMath.Tests/TestResults"
+    $ApprovalTestsDirectories = "$SolutionRoot/src/WpfMath.Tests/TestResults",
+    $VerifyDirectories = "$SolutionRoot/src/WpfMath.ApiTest/api"
 )
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
-Get-ChildItem $TestResultDirectory -Filter "*.received.txt" | ForEach-Object {
+Get-ChildItem $ApprovalTestsDirectories -Filter "*.received.txt" | ForEach-Object {
     $receivedTestResult = $_.FullName
     $approvedTestResult = $receivedTestResult.Replace('.received.txt', '.approved.txt')
+    Move-Item -Force -LiteralPath $receivedTestResult $approvedTestResult
+}
+
+Get-ChildItem $VerifyDirectories -Filter "*.received.*" | ForEach-Object {
+    $receivedTestResult = $_.FullName
+    $approvedTestResult = $receivedTestResult.Replace('.received.', '.verified.')
     Move-Item -Force -LiteralPath $receivedTestResult $approvedTestResult
 }
