ede1c728ab28017a3362e6e62511e6c88b3eeaa6
Friedrich von Never <friedrich@fornever.me>
2023-01-29 00:36:06 +0100

    (#17) Fix compilation after merge, round 1

--- src/WpfMath.Tests/WpfBrushTests.fs
+++ src/WpfMath.Tests/WpfBrushTests.fs
@@ -10,12 +10,12 @@ open WpfMath.Rendering
 [<Fact>]
 let ``The brush is expected to remain in the container``() =
     let expected = Brushes.Green :> Brush
-    let actual: SolidColorBrush = downcast WpfBrush.FromBrush(expected).Get()
+    let actual: SolidColorBrush = downcast WpfBrush.FromBrush(expected).Value
     Assert.Equal(expected, actual)
 
 [<Fact>]
 let ``It's expected that the right brush will be created by color``() =
-    let color = RgbaColor((byte)255, (byte)254, (byte)253, (byte)252)
+    let color = RgbaColor(byte 255, byte 254, byte 253, byte 252)
     let expected = SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B))
-    let actual: SolidColorBrush = downcast WpfBrush.FromColor(color).Get()
+    let actual: SolidColorBrush = downcast WpfBrushFactory.Instance.FromColor(color).ToWpf()
     Assert.Equal(expected.Color, actual.Color)
