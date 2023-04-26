d8abcdf69a16d17faadbe85dc6f41a6a35ea38f8
Orace <pierre.lando@gmail.com>
2023-04-26 17:05:10 +0200

    Improve WpfMath.Example layout

--- src/WpfMath.Example/MainWindow.xaml.cs
+++ src/WpfMath.Example/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
 using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using Microsoft.Win32;
+using Microsoft.Xaml.Behaviors.Core;
 using WpfMath.Converters;
 using WpfMath.Parsers;
 using WpfMath.Rendering;
@@ -17,28 +19,60 @@ public partial class MainWindow
 {
     private readonly TexFormulaParser _formulaParser = WpfTeXFormulaParser.Instance;
 
-    private static ComboBoxItem DemoFormula(string name, string text) =>
-        new ComboBoxItem { Content = name, DataContext = text };
-
-    private readonly IList<ComboBoxItem> _testFormulas = new[]
-    {
-        DemoFormula("Integral 1", @"\int_0^{\infty}{x^{2n} e^{-a x^2} \, dx} = \frac{2n-1}{2a} \int_0^{\infty}{x^{2(n-1)} e^{-a x^2} \, dx} = \frac{(2n-1)!!}{2^{n+1}} \sqrt{\frac{\pi}{a^{2n+1}}}"),
-        DemoFormula("Integral 2", @"\int_a^b{f(x) \, dx} = (b - a) \sum_{n = 1}^{\infty}  {\sum_{m = 1}^{2^n  - 1} { ( { - 1} )^{m + 1} } } 2^{ - n} f(a + m ( {b - a}  )2^{-n} )"),
-        DemoFormula("Integral 3", @"L = \int_a^\infty \sqrt[4]{ \left\vert \sum_{i,j=1}^ng_{ij}\left\(\gamma(t)\right\) \left\[\frac{d}{dt}x^i\circ\gamma(t) \right\] \left\{\frac{d}{dt}x^j\circ\gamma(t) \right\} \right\|} \, dt"),
-        DemoFormula("Number matrix", @"\matrix{4&78&3 \\ 5 & 9  & 82 }"),
-        DemoFormula("Nested matrix", @"\matrix{4&78&3\\ 57 & {\matrix{78 \\ 12}}  & 20782 }"),
-        DemoFormula("Cases", @"f(x) = \cases{1/3 & \mathrm{if} \thinspace 0\le x\le 1;\cr 2/3 & \mathrm{if} \thinspace 3\le x \le 4; \cr 0 & \mathrm{elsewhere.}\cr}"),
-        DemoFormula("Matrix and new lines", @"v \times w = \left( \matrix{v_2 w_3 - v_3 w_2 \\ v_3 w_1 - v_1 w_3 \\ v_1 w_2 - v_2 w_1} \right) \\ \matrix{\mathrm{where} & v= \left(\matrix{ v_1 \\ v_2 \\ v_3 }\right), \\ & w= \left( \matrix{w_1 \\ w_2  \\ w_3} \right)}"),
-        DemoFormula("Big matrix", @"\Gamma_{\mu \rho} ^{\sigma}= \pmatrix{\pmatrix{0 & 0 & 0 \\ 0 & -r & 0 \\ 0 & 0 & -r \sin^2(\theta)} \\ \pmatrix{0 & \frac{1}{r} & 0 \\ \frac{1}{r} & 0 & 0 \\ 0 & 0 & -\sin(\theta) \cos(\theta)} \\ \pmatrix{0 & 0 & \frac{1}{r} \\ 0 & 0 & \frac{1}{\tan(\theta)} \\ \frac{1}{r} & \frac{1}{\tan(\theta)} & 0 }}"),
-        DemoFormula("Environment with Matrix", @"\begin{pmatrix} a_1 & a_2 & a_3 \\ b_1 & b_2 & b_3 \\ c_1 & c_2 & c_3 \end{pmatrix}")
-    };
-
     public MainWindow()
     {
-        InitializeComponent();
+        ApplyPresetCommand = new ActionCommand(ApplyPreset);
+        DemoFormulas = new ObservableCollection<DemoFormula>
+        {
+            new ("Integral 1", @"\int_0^{\infty}{x^{2n} e^{-a x^2} \, dx} = \frac{2n-1}{2a} \int_0^{\infty}{x^{2(n-1)} e^{-a x^2} \, dx} = \frac{(2n-1)!!}{2^{n+1}} \sqrt{\frac{\pi}{a^{2n+1}}}"),
+            new ("Integral 2", @"\int_a^b{f(x) \, dx} = (b - a) \sum_{n = 1}^{\infty}  {\sum_{m = 1}^{2^n  - 1} { ( { - 1} )^{m + 1} } } 2^{ - n} f(a + m ( {b - a}  )2^{-n} )"),
+            new ("Integral 3", @"L = \int_a^\infty \sqrt[4]{ \left\vert \sum_{i,j=1}^ng_{ij}\left\(\gamma(t)\right\) \left\[\frac{d}{dt}x^i\circ\gamma(t) \right\] \left\{\frac{d}{dt}x^j\circ\gamma(t) \right\} \right\|} \, dt"),
+            new ("Number matrix", @"\matrix{4&78&3 \\ 5 & 9  & 82 }"),
+            new ("Nested matrix", @"\matrix{4&78&3\\ 57 & {\matrix{78 \\ 12}}  & 20782 }"),
+            new ("Cases", @"f(x) = \cases{1/3 & \mathrm{if} \thinspace 0\le x\le 1;\cr 2/3 & \mathrm{if} \thinspace 3\le x \le 4; \cr 0 & \mathrm{elsewhere.}\cr}"),
+            new ("Matrix and new lines", @"v \times w = \left( \matrix{v_2 w_3 - v_3 w_2 \\ v_3 w_1 - v_1 w_3 \\ v_1 w_2 - v_2 w_1} \right) \\ \matrix{\mathrm{where} & v= \left(\matrix{ v_1 \\ v_2 \\ v_3 }\right), \\ & w= \left( \matrix{w_1 \\ w_2  \\ w_3} \right)}"),
+            new ("Big matrix", @"\Gamma_{\mu \rho} ^{\sigma}= \pmatrix{\pmatrix{0 & 0 & 0 \\ 0 & -r & 0 \\ 0 & 0 & -r \sin^2(\theta)} \\ \pmatrix{0 & \frac{1}{r} & 0 \\ \frac{1}{r} & 0 & 0 \\ 0 & 0 & -\sin(\theta) \cos(\theta)} \\ \pmatrix{0 & 0 & \frac{1}{r} \\ 0 & 0 & \frac{1}{\tan(\theta)} \\ \frac{1}{r} & \frac{1}{\tan(\theta)} & 0 }}"),
+            new ("Environment with Matrix", @"\begin{pmatrix} a_1 & a_2 & a_3 \\ b_1 & b_2 & b_3 \\ c_1 & c_2 & c_3 \end{pmatrix}")
+        };
 
-        FormulaSelector.ItemsSource = _testFormulas;
-        FormulaSelector.SelectedIndex = 0;
+        InitializeComponent();
+    }
+
+    public ICommand ApplyPresetCommand { get; }
+    public ObservableCollection<DemoFormula> DemoFormulas { get; }
+
+    private void ApplyPreset(object obj)
+    {
+        if (obj is string s)
+            InputTextBox.Text = s;
+    }
+
+    private static string AddSvgHeader(string svgText)
+    {
+        var builder = new StringBuilder();
+        builder.AppendLine("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>")
+               .AppendLine("<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" >")
+               .AppendLine(svgText)
+               .AppendLine("</svg>");
+
+        return builder.ToString();
+    }
+
+    private void ExitMenuOnClick(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void FormulaTextBoxOnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var item = (ComboBoxItem) ((ComboBox) sender).SelectedItem;
+        InputTextBox.Text = (string)item.DataContext;
+    }
+
+    private void InputTextBoxOnSelectionChanged(object sender, RoutedEventArgs e)
+    {
+        Formula.SelectionStart = InputTextBox.SelectionStart;
+        Formula.SelectionLength = InputTextBox.SelectionLength;
     }
 
     private TexFormula? ParseFormula(string input)
@@ -58,7 +92,7 @@ public partial class MainWindow
         return formula;
     }
 
-    private void saveButton_Click(object sender, RoutedEventArgs e)
+    private void SaveMenuOnClick(object sender, RoutedEventArgs e)
     {
         // Choose file
         SaveFileDialog saveFileDialog = new SaveFileDialog()
@@ -101,32 +135,16 @@ public partial class MainWindow
                 return;
         }
     }
+}
 
-    private string AddSvgHeader(string svgText)
+public class DemoFormula
+{
+    public DemoFormula(string name, string formula)
     {
-        var builder = new StringBuilder();
-        builder.AppendLine("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>")
-            .AppendLine("<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" >")
-            .AppendLine(svgText)
-            .AppendLine("</svg>");
-
-        return builder.ToString();
+        Name = name;
+        Formula = formula;
     }
 
-    private void Window_Closed(object sender, EventArgs e)
-    {
-        //
-    }
-
-    private void inputTextBox_SelectionChanged(object sender, RoutedEventArgs e)
-    {
-        Formula.SelectionStart = InputTextBox.SelectionStart;
-        Formula.SelectionLength = InputTextBox.SelectionLength;
-    }
-
-    private void FormulaTextBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        var item = (ComboBoxItem) ((ComboBox) sender).SelectedItem;
-        InputTextBox.Text = (string)item.DataContext;
-    }
+    public string Name { get; }
+    public string Formula { get; }
 }
