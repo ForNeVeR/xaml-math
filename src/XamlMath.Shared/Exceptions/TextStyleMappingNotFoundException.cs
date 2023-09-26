73affa78f8d44131b5ae9ae02a57398e3d01a73d
Lehonti Ramos <lehonti@ramos>
2023-09-26 21:40:49 +0200

    Converted `string.Format` to interpolated string

--- src/XamlMath.Shared/Exceptions/TextStyleMappingNotFoundException.cs
+++ src/XamlMath.Shared/Exceptions/TextStyleMappingNotFoundException.cs
@@ -3,7 +3,7 @@ namespace XamlMath.Exceptions;
 public sealed class TextStyleMappingNotFoundException : TexException
 {
     internal TextStyleMappingNotFoundException(string textStyleName)
-        : base(string.Format("Cannot find mapping for the style with name '{0}'.", textStyleName))
+        : base($"Cannot find mapping for the style with name '{textStyleName}'.")
     {
     }
 }
--- src/XamlMath.Shared/FormulaNotFoundException.cs
+++ src/XamlMath.Shared/FormulaNotFoundException.cs
@@ -5,7 +5,7 @@ namespace XamlMath;
 public sealed class FormulaNotFoundException : Exception
 {
     internal FormulaNotFoundException(string formulaName)
-        : base(string.Format("Cannot find predefined formula with name '{0}'.", formulaName))
+        : base($"Cannot find predefined formula with name '{formulaName}'.")
     {
     }
 }
